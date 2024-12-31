d6506e79f385feb94298580b0f2c3352847e76eb
陳炫樺 <chenxuanhua456@gmail.com>
2024-12-31 22:11:20 +0800

    加入 monolog 測試程式碼

--- public/index.php
+++ public/index.php
@@ -13,3 +13,15 @@ $hello= new Demo\HelloWorld();
 
 use Demo\HelloWorld as World;
 $world = new World();
+//Monolog
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
